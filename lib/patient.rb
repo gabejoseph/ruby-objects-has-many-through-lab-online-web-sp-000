31ac5d98001b14ea349b5577cfcf46a93b447a23
gabriel.joseph4 <gabriel.joseph4@gmail.com>
2020-04-29 15:10:43 +0000

    Automatically backed up by Learn

---
+++ lib/patient.rb
@@ -0,0 +1,13 @@
+class Patient 
+  
+  attr_accessor :date, :doctor 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name 
+    @@all << self 
+  end 
+  
+  def new_appointment(date, doctor)
+    newappointment = 
